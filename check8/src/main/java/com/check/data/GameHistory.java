eb2fdd8c844ad77b4b8d2aa72f1dde0a4df3d20b
Elle0-0 <andreamichellevaz@gmail.com>
2025-02-07 20:52:16 +0000

    Added data package with memento design pattern

---
+++ check8/src/main/java/com/check/data/GameHistory.java
@@ -0,0 +1,20 @@
+package com.check.data;
+
+import java.util.Stack;
+
+//caretaker
+public class GameHistory {
+    private final Stack<GameCache> history = new Stack<>();
+
+    public void save(GameData gameData) {
+        history.push(gameData.save());
+    }
+
+    public void undo(GameData character) {
+        if (!history.isEmpty()) {
+            character.restore(history.pop());
+        } else {
+            System.out.println("No saves to restore!");
+        }
+    }
+}

3ced80e096563f016689a0085023cedc58b848e9
ElliceNels <23371323@studentmail.ul.ie>
2025-02-07 14:36:24 +0000

    [Feat] Add character tests

---
+++ check8/src/test/java/com/check/characters/BruteTest.java
@@ -0,0 +1,42 @@
+package com.check.characters;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BruteTest {
+
+    @Test
+    public void testCPUFalse() {
+        Brute brute = new Brute(false);
+        assertFalse(brute.isCPU());
+    }
+
+    @Test
+    public void testCPUTrue() {
+        Brute brute = new Brute(true);
+        assertTrue(brute.isCPU());
+    }
+
+    @Test
+    public void testDescription() {
+        Brute brute = new Brute(true);
+        assertEquals("""
+        A brute character who uses his strength to defeat his enemies.
+        He is a master of close combat and can overpower any enemy that comes his way.
+        He is a fierce and powerful warrior who can crush his enemies with his bare hands and large axe.
+        """, brute.getDescription());
+    }
+
+    @Test
+    public void testCharacterName() {
+        Brute brute = new Brute(true);
+        assertEquals("Brute", brute.getName());
+    }
+
+    @Test
+    public void testWeapon() {
+        Brute brute = new Brute(true);
+        assertEquals(com.check.items.Axe.class, brute.getWeapon().getClass());
+    }
+    
+}
---
+++ check8/src/test/java/com/check/characters/MageTest.java
@@ -0,0 +1,42 @@
+package com.check.characters;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MageTest {
+
+    @Test
+    public void testWeapon() {
+        Mage mage = new Mage(true);
+        assertEquals(com.check.items.Staff.class, mage.getWeapon().getClass());
+    }
+
+    @Test
+    public void testCPUTrue() {
+        Mage mage = new Mage(true);
+        assertTrue(mage.isCPU());
+    }
+
+    @Test
+    public void testCPUFalse() {
+        Mage mage = new Mage(false);
+        assertFalse(mage.isCPU());
+    }
+
+    @Test
+    public void testDescription() {
+        Mage mage = new Mage(true);
+        assertEquals("""
+        A powerful mage who uses the power of the elements to defeat his enemies.
+        He is a master of the arcane arts and can cast powerful spells to destroy his foes.
+        He is a wise and cunning warrior who uses his intelligence to outsmart his enemies.
+        """, mage.getDescription());
+    }
+
+    @Test
+    public void testCharacterName() {
+        Mage mage = new Mage(true);
+        assertEquals("Mage", mage.getName());
+    }
+    
+}

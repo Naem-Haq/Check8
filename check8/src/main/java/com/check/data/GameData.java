ce946ab14dd7c70702ce344c3fc78dcd5cb3cdb6
Elle0-0 <andreamichellevaz@gmail.com>
2025-02-07 22:23:02 +0000

    Updated with characters health and test case

--- check8/src/main/java/com/check/data/GameData.java
+++ check8/src/main/java/com/check/data/GameData.java
@@ -2,21 +2,35 @@ package com.check.data;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
-//Originator
+import com.check.characters.Character;
+
+
+//Memento Design Pattern - Originator
 public class GameData {
-    private Character character;
-    public void setState(Character character) {
-        this.character = character;
+    private Character player1;
+    private Character player2;
+    public void setState(Character player1, Character player2) {
+        this.player1 = player1;
+        this.player2 = player2;
     }
+    //will update with portions once class is implemented
     public GameCache save() {
         Map<String, Object> state = new HashMap<>();
-//        state.put("health", character.getHealthBar()
+        state.put("player1_health", player1.getHealthBar().getHealth());
+        state.put("player2_health", player2.getHealthBar().getHealth());
         return new GameCache(state);
     }
     public void restore(GameCache gameCache) {
         Map<String, Object> state = gameCache.getState();
-//        this.character.setHealth((int) state.get("health"));
+        player1.getHealthBar().setHealth((int) state.get("player1_health"));
+        player2.getHealthBar().setHealth((int) state.get("player2_health"));
     }
+
+    public void printState() {
+        System.out.println("Player 1: " + player1.getName() + " | Health: " + player1.getHealthBar().getHealth());
+        System.out.println("Player 2: " + player2.getName() + " | Health: " + player2.getHealthBar().getHealth());
+    }
+
+    
 }

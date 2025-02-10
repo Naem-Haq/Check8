3ea296ebbb9cbd287de8533ea75f48f4eba13ed2
Elle0-0 <andreamichellevaz@gmail.com>
2025-02-10 21:43:57 +0000

    updated to have multiple players

--- check8/src/test/java/com/check/data/GameDataTest.java
+++ check8/src/test/java/com/check/data/GameDataTest.java
@@ -7,6 +7,8 @@ import com.check.characters.Character;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameDataTest {
 
@@ -15,25 +17,36 @@ public class GameDataTest {
         GameData gameData = new GameData();
         GameHistory history = new GameHistory();
 
+        // Create characters
         Character player1 = new Knight(false);
         Character player2 = new Brute(true);
         player1.getHealthBar().setHealth(100);
         player2.getHealthBar().setHealth(100);
 
-        gameData.setState(player1, player2);
+        // Create a list of players and set the game state
+        List<Character> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+
+        gameData.setState(players);
         history.save(gameData);
 
+        // Modify health
         player1.getHealthBar().setHealth(30);
         player2.getHealthBar().setHealth(50);
 
-        gameData.setState(player1, player2);
+        gameData.setState(players);
         history.save(gameData);
 
+        // Further modify health
         player1.getHealthBar().setHealth(20);
         player2.getHealthBar().setHealth(40);
 
+        // Undo to the previous state
         history.undo(gameData);
+
+        // Check that the health has been restored correctly
         assertEquals(30, player1.getHealthBar().getHealth());
-        assertEquals(50, player2.getHealthBar().getHealth()); 
+        assertEquals(50, player2.getHealthBar().getHealth());
     }
 }

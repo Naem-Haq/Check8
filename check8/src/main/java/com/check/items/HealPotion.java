fdfddc224c4176b5643661dc0877ede9132dcd74
ElliceNels <23371323@studentmail.ul.ie>
2025-02-10 21:08:09 +0000

    [Feat] Implement potions

--- check8/src/main/java/com/check/items/HealPotion.java
+++ check8/src/main/java/com/check/items/HealPotion.java
@@ -1,5 +1,20 @@
 package com.check.items;
+import com.check.characters.Character;
 
-public class HealPotion extends Potion{
+public class HealPotion extends Potion{ 
+
+    public HealPotion() {
+        super(20);
+    }
+
+    @Override
+    public String useText(){
+        return "You drink a healing potion!";
+    }
+
+    @Override
+    public void use(Character potionOwner) {
+        potionOwner.getHealthBar().increaseHealth(this.getHealthPoints());
+    }
     
 }
--- check8/src/main/java/com/check/items/Potion.java
+++ check8/src/main/java/com/check/items/Potion.java
@@ -1,5 +1,17 @@
 package com.check.items;
 
 public abstract class Potion extends Item {
+
+    private int healthPoints;
+
+    public Potion(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public abstract String useText();
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
     
 }

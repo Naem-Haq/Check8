7a00cfadac35ed33175d6b8f25ebdb4287c58451
Naem-Haq <23379243@studentmail.ul.ie>
2025-02-14 10:37:38 +0000

    [Rebase] commit to stash

--- check8/src/main/java/com/check/game/CPU.java
+++ check8/src/main/java/com/check/game/CPU.java
@@ -5,13 +5,13 @@ class CPU {
 
     /* Method commented out - pending Command for attack and dodge implementation */
 
-    // public static void generateMove(com.check.characters.Character character) {
-    //     CharacterCommand action;
-    //     if (character.getHealthBar().getHealth() < 50) {
-    //         action = new DodgeCommand(character);
-    //     } else {
-    //         action = new AttackCommand(character);
-    //     }
-    //     action.execute();
-    // }
+    public static void generateMove(com.check.characters.Character character) {
+        CharacterCommand action;
+        if (character.getHealthBar().getHealth() < 50) {
+            action = new DodgeCommand(character);
+        } else {
+            action = new AttackCommand(character);
+        }
+        action.execute();
+    }
 }

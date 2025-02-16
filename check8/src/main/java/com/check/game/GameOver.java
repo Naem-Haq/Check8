0861345ce4b835692b7da967733a2f9fb21a5348
Naem-Haq <23379243@studentmail.ul.ie>
2025-02-16 22:24:30 +0000

    [Feat] Game Over state added

---
+++ check8/src/main/java/com/check/game/GameOver.java
@@ -0,0 +1,8 @@
+package com.check.game;
+
+public class GameOver implements GameState {
+    @Override
+    public void handleRequest(Game game) {
+        // Game is over, handle end game logic
+    }
+} 

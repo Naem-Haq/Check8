f214371dc931632e0b14fe2fc2da8653146452ed
Naem-Haq <23379243@studentmail.ul.ie>
2025-02-17 09:23:33 +0000

    [Fix] Game states fixed

--- check8/src/main/java/com/check/game/InProgress.java
+++ check8/src/main/java/com/check/game/InProgress.java
@@ -1,8 +1,18 @@
 package com.check.game;
 
-public class InProgress extends GameState {
+public class InProgress implements GameState {
+    @Override
     public void handleRequest(Game game) {
-        System.out.println("Game is already in progress.");
+        // Game is in progress, handle game logic
+        game.incrementRounds();
+        if (isGameOver(game)) {
+            game.setState(new GameOver());
+        }
+    }
+
+    private boolean isGameOver(Game game) {
+        return game.getPlayer().getHealthBar().getHealth() <= 0 || 
+               game.getCPU().getHealthBar().getHealth() <= 0;
     }
 }
 

eb07c16ec781b415e80b8d6ef32441776926f9fb
Naem-Haq <23379243@studentmail.ul.ie>
2025-02-17 22:22:50 +0000

    [Refactor] Update Chain of Responsibility pattern to respect dodge limits

--- check8/src/main/java/com/check/game/CriticalHPHandler.java
+++ check8/src/main/java/com/check/game/CriticalHPHandler.java
@@ -1,6 +1,7 @@
 package com.check.game;
 
 import com.check.characters.Character;
+import com.check.characters.DodgeCommand;
 
 public class CriticalHPHandler extends Handler {
     @Override
@@ -8,9 +9,16 @@ public class CriticalHPHandler extends Handler {
         double healthPercentage = (double) character.getHealthBar().getHealth() / character.getHealthBar().getMaxHealth() * 100;
         if (healthPercentage < 25) {
             System.out.println("Character at critical health, taking defensive action.");
-            character.setAttackable(false);
-        }
-        if (nextHandler != null) {
+            if (character.canDodge()) {
+                character.setAttackable(false);
+                new DodgeCommand(character).execute(character);
+            } else {
+                // If can't dodge, try next strategy
+                if (nextHandler != null) {
+                    nextHandler.handleCharacterDecision(character);
+                }
+            }
+        } else if (nextHandler != null) {
             nextHandler.handleCharacterDecision(character);
         }
     }

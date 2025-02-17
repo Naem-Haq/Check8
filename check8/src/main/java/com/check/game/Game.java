3a59b89ed3f0db755b02708bb0406641d7e94bf7
Naem-Haq <23379243@studentmail.ul.ie>
2025-02-17 09:24:07 +0000

    [Feat] CoR implemented

--- check8/src/main/java/com/check/game/Game.java
+++ check8/src/main/java/com/check/game/Game.java
@@ -1,54 +1,71 @@
 package com.check.game;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.check.characters.Character;
 import com.check.data.GameCache;
-
-import java.util.List;
-import java.util.ArrayList;
+import com.check.characters.CharacterCreator;
 
 public class Game {
+    private static Logger logger = LoggerFactory.getLogger(Game.class.getName());
     private List<Character> characters;
-    private int rounds;
     private GameState state;
+    private int rounds;
+    private Character player;
+    private Character cpu;
 
     public Game() {
         this.characters = new ArrayList<>();
         this.state = new Ready();
         this.rounds = 0;
+        logger.info("New game initialized");
     }
 
     public void setState(GameState state) {
         this.state = state;
+        logger.debug("Game state changed to: {}", state.getClass().getSimpleName());
     }
 
     public void request() {
         state.handleRequest(this);
     }
 
-
-    public void setCacheState(List<Character> characters, GameState state, int rounds) {
-        this.characters = new ArrayList<>(characters);
-        this.state = state;
-        this.rounds = rounds;
-    }
-    public GameCache saveToCache() {
-        System.out.println("Game state saved.");
-        return new GameCache(getCharacters(),getState(), getRounds());
-    }
-
-    public void restoreFromCache(GameCache cache) {
-        this.characters = cache.getCharacters();
-        this.rounds = cache.getRounds();
-        this.state = cache.getState();
-        System.out.println("Game state restored.");
-    }
-
-    public GameState getState() {
+    public GameState getCurrentState() {
         return state;
     }
 
-    public List<Character> getCharacters() {
-        return characters;
+    public void initializeGame(String playerCharType, String cpuCharType) throws CharacterCreator.InvalidCharacterException {
+        player = CharacterCreator.createCharacter(playerCharType, false);
+        cpu = CharacterCreator.createCharacter(cpuCharType, true);
+        characters.add(player);
+        characters.add(cpu);
+        setState(new InProgress());
+        logger.info("Game initialized with {} vs {}", playerCharType, cpuCharType);
+    }
+
+    public Character getPlayer() {
+        return player;
+    }
+
+    public Character getCPU() {
+        return cpu;
+    }
+
+    public GameCache saveToCache() {
+        return new GameCache(characters, state, rounds);
+    }
+
+    public void restoreFromCache(GameCache cache) {
+        this.characters = new ArrayList<>(cache.getCharacters());
+        this.state = cache.getState();
+        this.rounds = cache.getRounds();
+        logger.info("Game restored from cache");
+    }
+
+    public void incrementRounds() {
+        rounds++;
     }
 
     public int getRounds() {

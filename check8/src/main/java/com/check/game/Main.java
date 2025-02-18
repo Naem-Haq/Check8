fda84265a92fdbafb33c76383d5cb7ad1331c5de
Naem-Haq <23379243@studentmail.ul.ie>
2025-02-18 10:18:46 +0000

    [Chore] Update resource files and main class

--- check8/src/main/java/com/check/game/Main.java
+++ check8/src/main/java/com/check/game/Main.java
@@ -2,7 +2,6 @@ package com.check.game;
 
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.start();
+        Game.getInstance().start();
     }
 } 
